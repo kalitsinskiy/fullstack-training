1843074479afce5fad2aef180bb650b6f6ed4ff3
oleksandrkrk <oleksandr.krysiuk@nice.com>
2026-08-10 12:34:19 +0300

    refactor: remove unused exports and dead dependencies found by knip

--- santa-app/src/components/PageSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageSpinner() {
-  return (
-    <div className="flex min-h-[40vh] items-center justify-center">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-muted border-t-primary" />
-    </div>
-  );
-}
--- santa-app/src/pages/WishlistPage.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router";
-
-export default function WishlistPage() {
-  const { id } = useParams();
-  return <h1>Wishlist for room {id}</h1>;
-}
