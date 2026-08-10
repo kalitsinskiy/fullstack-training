b1ad61f8f5d3c1f71f34478cadb36d85fd83ea71
yuri-sokyrko <yuri.sokyrko@gmail.com>
2026-08-10 16:06:53 +0300

    refactor(app): build axios clients from one factory

--- santa-app/src/lib/notificationsApi.ts
+++ santa-app/src/lib/notificationsApi.ts
@@ -1,32 +1,3 @@
-import axios, { AxiosError } from 'axios';
-import { tokenStore } from './api';
+import { createApiClient } from './create-api-client';
 
-export const notificationsApi = axios.create({
-  baseURL: import.meta.env.VITE_WS_URL || undefined,
-  headers: { 'Content-Type': 'application/json' },
-});
-
-notificationsApi.interceptors.request.use((config) => {
-  const token = tokenStore.get();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-notificationsApi.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => {
-    if (error.response?.status === 401) {
-      tokenStore.clear();
-
-      if (window.location.pathname !== '/login') {
-        window.location.assign('/login');
-      }
-    }
-
-    return Promise.reject(error);
-  },
-);
+export const notificationsApi = createApiClient(import.meta.env.VITE_WS_URL);
